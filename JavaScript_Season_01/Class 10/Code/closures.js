f878f9806ce2743c19b35955ebd2e2820b792999
Suraj <suraj.ukumar.p@gmail.com>
2024-08-15 17:28:27 +0530

    fix: The previous files and folders are deleted.

--- JavaScript_Season_01/Class 10/Code/closures.js
+++
@@ -1,6 +0,0 @@
-function x() {
-  var i = 10;
-  setTimeout(() => {
-    console.log(i);
-  }, 1000);
-}
--- JavaScript_Season_01/Class 17/Code/setTime(0).js
+++
@@ -1,7 +0,0 @@
-console.log("start");
-
-setTimeout(function cb() {
-  console.log("Callback");
-}, 0); // here i put the setime = 0,
-
-console.log("end");
